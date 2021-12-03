92830e6755472584cee9f5b6e6fc4dd6e36d8d78
nithinsikinam <nithinsikinam@gmail.com>
2021-12-03 21:44:24 +0530

    commited two

--- app/src/main/java/com/example/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/myapplication/HomeScreen.java
@@ -3,16 +3,20 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class HomeScreen extends AppCompatActivity {
@@ -27,21 +31,29 @@ public class HomeScreen extends AppCompatActivity {
         String url ="http://27.6.130.5:12345/json/sample.json";
 
 // Request a string response from the provided URL.
-        val jsonObjectRequest = JsonObjectRequest(
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET, url, null,
-                Response.Listener {
-                   response->
-                           val url=response.getString ("data")
-                    },
-                },  Response.ErrorListener() {
+               new Response.Listener<JSONObject>() {
+                   @Override
+                           public void onResponse (JSONObject response){
+                       try {
+                           String url =response.getString("data");
+                           Log.d("Response","url");
+                       } catch (JSONException e) {
+                           e.printStackTrace();
+                       }
+                   }
+                },  new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
+                Log.d(
+                        "Error","Fetching Error"
+                );
             }
         });
 
 // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
 
 
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,7 +17,22 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
@@ -26,6 +41,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://27.6.130.5:12345/json/sample.json";
+
+// Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse (JSONObject response){
+                        try {
+                            String url =response.getString("data");
+                            Log.d("Response","url");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },  new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(
+                        "Error","Fetching Error"
+                );
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
 
 
     }
